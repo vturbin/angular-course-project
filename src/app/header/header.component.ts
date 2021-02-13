5cc26c7db2a8511defa8df62fbcdb0f2e779a3cf
vturbin <v.turbin@hotmail.com>
2021-02-13 10:57:54 +0100

    add routing to recipes

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,14 +1,9 @@
-import { Component, EventEmitter, Output} from "@angular/core";
+import { Component} from "@angular/core";
 
 @Component({
     templateUrl: './header.component.html',
     selector: 'app-header'
 })
 export class HeaderComponent {
-    @Output() featureSelected = new EventEmitter<string>();
     collapsed = true;
-
-    onSelect(feature: string) {
-        this.featureSelected.emit(feature);
-    }
 }
